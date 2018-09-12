e5ef88b46129775e6f0766100ce9cd0d109d8f94
Joe Denny <josephpauldenny@gmail.com>
2018-09-12 17:54:00 -0400

    add Redux-ORM

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -4,32 +4,35 @@ import {
     Container,
 } from "semantic-ui-react";
 
+import './App.css';
+
 import TabBarContainer from "features/tabs/TabBar";
 import UnitInfo from "features/unitInfo/UnitInfo";
 import Pilots from "features/pilots/Pilots";
 import Mechs from "features/mechs/Mechs";
 import UnitOrganization from "features/unitOrganization/UnitOrganization";
+import Tools from "features/tools/Tools";
 
-import './App.css';
 
 class App extends Component {
     render() {
         const tabs = [
-          {name : "unitInfo", label : "Unit Info", component : UnitInfo,},
-          {name : "pilots", label : "Pilots", component : Pilots,},
-          {name : "mechs", label : "Mechs", component : Mechs,},
-          {name : "unitOrganization", label : "Unit Organization", component : UnitOrganization}
+            {name : "unitInfo", label : "Unit Info", component : UnitInfo,},
+            {name : "pilots", label : "Pilots", component : Pilots,},
+            {name : "mechs", label : "Mechs", component : Mechs,},
+            {name : "unitOrganization", label : "Unit Organization", component : UnitOrganization},
+            {name : "tools", label : "Tools", component : Tools},
         ];
 
         return (
-          <div className="App">
-            <div className="App-header">
-                <Header inverted as="h1">Folk Music of the British Isles</Header>
+            <div className="App">
+                <div className="App-header">
+                    <Header inverted as="h1">Project Mini-Mek</Header>
+                </div>
+                <Container>
+                    <TabBarContainer tabs={tabs} size="massive" />
+                </Container>
             </div>
-            <Container>
-                <TabBarContainer tabs={tabs} size="massive" />
-            </Container>
-        </div>
         );
     }
 }
